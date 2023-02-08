c7308009c691a24b6889bb69565a156f2721d48b
coderchrisdean <coderchrisdean@gmail.com>
2023-02-08 14:25:21 -0800

    started writing dealer function logic

---
+++ db/playerFunctionsDb.js
@@ -0,0 +1,36 @@
+//playerFunctionsDb.js
+// Methods to communicate with the database for player functions
+
+const { Player } = require('../models');
+// define the Player model
+// const playerAction  = sequelize.define('playerAction', {
+
+//     hand: {
+//         type: Sequelize.JSON,
+//         allowNull: false,
+//     },
+//     bet: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false,
+//     },
+    
+
+// });
+// playerAction.prototype.hit = function () {
+//     //use the hit function from playerFunctions.js
+//     this.hand = hit(this.hand);
+//     this.save();
+//     return this.hand;
+// };
+
+// add a method to the player model to handle stand
+Player.prototype.stand = function () {
+    //use the stand function from playerFunctions.js
+    this.hand = stand(this.hand);
+    this.save();
+    return this.hand;
+};
+
+// add a method to the player model to handle double
+
+module.exports = playerFunctionsDb;
