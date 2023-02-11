5ae62282fe77c775a357c7cb6fad5fc253204f54
coderchrisdean <coderchrisdean@gmail.com>
2023-02-11 13:18:11 -0800

    updated Deck.js to recognize Ace as 1 or 11

--- src/helpers/class/Deck.js
+++ src/helpers/class/Deck.js
@@ -1,28 +1,61 @@
-const path = require('path');
-// const deckOfCards = require(path.join(__dirname, "../../db/activeShoe.json"));
-class Deck {
-    constructor() {
-      this.shoe = require(path.join(__dirname, "../../db/activeShoe.json"));
-      // this.shoe = deckOfCards
-      // this.usedShoe = []
-    }
-  
-    async drawCard() {
-      const length = this.shoe.cards.length;
-      const r = Math.floor(Math.random() * length);
-      const cardSelected = await this.shoe.cards[r];
-      this.shoe.cards.splice(r, 1);
-  
-      return cardSelected;
-    }
+const path = require("path");
+const fs = require("fs");
+const activeShoe = require(path.join(__dirname, "../../db/activeShoe.json"));
 
-    handValue(hand) {
-        let total = 0;
-        for (let card of hand) {
-            total += card.value;
-        }
-        return total;
-    }
+class Deck {
+  constructor() {
+    this.shoe = activeShoe;
+    this.usedShoe = [];
   }
 
+  async drawCard() {
+    const length = this.shoe.cards.length;
+    const r = Math.floor(Math.random() * length);
+    const cardSelected = this.shoe.cards[r];
+    this.shoe.cards.splice(r, 1);
+    this.usedShoe.push(cardSelected);
+    fs.writeFile(
+      path.join(__dirname, "../../db/activeShoe.json"),
+      JSON.stringify(this.shoe),
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("The file has been saved!");
+      }
+    );
+    
+    return cardSelected;
+    
+  }
+
+  handValue(hand) {
+    let total = 0;
+    let aceCount = 0;
+  
+    for (let card of hand) {
+      if (card.name === "Ace") {
+        aceCount++;
+      } else {
+        total += card.value;
+      }
+    }
+  
+    for (let i = 0; i < aceCount; i++) {
+      if (total + 11 <= 21) {
+        total += 11;
+      } else {
+        total += 1;
+      }
+    }
+  
+    return total;
+  }
+  
+  }
+  }
+  
   module.exports = Deck;
+
+
