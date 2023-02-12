df48439038d10e39fa381f48af6952b85677c13e
coderchrisdean <coderchrisdean@gmail.com>
2023-02-12 03:53:54 -0800

    added handlebars helper for card.  updated deck builder to build the html string when building a new deck

--- src/helpers/deck.js
+++ src/helpers/deck.js
@@ -1,44 +1,56 @@
+const Handlebars = require("handlebars");
+
 class Deck {
-	constructor(options = {}) {
-	  const defaultOptions = { numberOfDecks: 1 };
-	  this.opts = { ...defaultOptions, ...options };
-	  this.cards = this.createDeck();
-	}
-  
-	createDeck() {
-	  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
-	  const names = [
-		{ name: "Ace", value: 1 || 11 },
-		{ name: "2", value: 2 },
-		{ name: "3", value: 3 },
-		{ name: "4", value: 4 },
-		{ name: "5", value: 5 },
-		{ name: "6", value: 6 },
-		{ name: "7", value: 7 },
-		{ name: "8", value: 8 },
-		{ name: "9", value: 9 },
-		{ name: "10", value: 10 },
-		{ name: "Jack", value: 10 },
-		{ name: "Queen", value: 10 },
-		{ name: "King", value: 10 },
-	  ];
-	  let deck = [];
-	  let id = 1;
-  
-	  for (let d = 0; d < this.opts.numberOfDecks; d++) {
-		suits.forEach((suit) => {
-		  names.forEach((name) => {
-			deck.push({
-			  id: id++,
-			  name: name.name,
-			  suit: suit,
-			  value: name.value,
-			});
-		  });
-		});
-	  }
-	  return deck;
-	}
+  constructor(options = {}) {
+    const defaultOptions = { numberOfDecks: 1 };
+    this.opts = { ...defaultOptions, ...options };
+    this.cards = this.createDeck();
   }
-  module.exports = Deck;
-  
+
+  createDeck() {
+    const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+    const names = [
+      { name: "Ace", value: 1 || 11 },
+      { name: "2", value: 2 },
+      { name: "3", value: 3 },
+      { name: "4", value: 4 },
+      { name: "5", value: 5 },
+      { name: "6", value: 6 },
+      { name: "7", value: 7 },
+      { name: "8", value: 8 },
+      { name: "9", value: 9 },
+      { name: "10", value: 10 },
+      { name: "Jack", value: 10 },
+      { name: "Queen", value: 10 },
+      { name: "King", value: 10 },
+    ];
+
+    Handlebars.registerHelper("card", function (value, suit) {
+      var html = `<div class="card ${value} ${suit}" id=c${id}></div>`;
+	  const htmlToLowerCase = html.toLowerCase();
+      return new Handlebars.SafeString(htmlToLowerCase)
+    });
+
+    let deck = [];
+    let id = 1;
+
+    for (let d = 0; d < this.opts.numberOfDecks; d++) {
+      suits.forEach((suit) => {
+        names.forEach((name) => {
+          const card = {
+            id: id++,
+            name: name.name,
+            suit: suit,
+            value: name.value,
+			html: Handlebars.helpers.card(name.name, suit)
+		  };
+     
+          deck.push(card);
+        });
+      });
+    }
+    return deck;
+  }
+}
+
+module.exports = Deck;
