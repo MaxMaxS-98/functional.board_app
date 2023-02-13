f423a3989effd688f020166613b5c9ec42b4c946
Lian Liu <riceudon@gmail.com>
2023-02-13 10:16:41 -0800

    Added turn propety in player model

--- models/Player.js
+++ models/Player.js
@@ -34,6 +34,11 @@ Player.init(
             type: DataTypes.STRING,
             allowNull: false,
             defaultValue: 'idle'
+        },
+        turn: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
         }
     },
     {
