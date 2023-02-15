9d036b51286c235fa2f92d5972b2fcf34e20e0ac
coderchrisdean <coderchrisdean@gmail.com>
2023-02-15 11:07:35 -0800

    fixed deckQueue, reverted deck/buildDeck.js to local capability. functions/buildDeck.js still server based.

--- src/functions/deck/buildDeck.js
+++ src/functions/deck/buildDeck.js
@@ -1,27 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 const Deck = require('../../helpers/deck');
-const url = 'http://localhost:3001/api/table/1'
-const herokuUrl = 'https://blackjack-blitz-test.herokuapp.com/api/table/1'
-const buildDeck = async () => {
+// const url = 'http://localhost:3001/api/table/1'
+// const herokuUrl = 'https://blackjack-blitz-test.herokuapp.com/api/table/1'
+// const buildDeck = async () => {
   
-  const buildDeck = async () => {
-    const activeShoe = new Deck();
-    const response = await fetch(herokuUrl, {
-      method: 'PUT',
-      body: JSON.stringify(activeShoe),
-      headers: { 'Content-Type': 'application/json' },
-    });
+//   const buildDeck = async () => {
+//     const activeShoe = new Deck();
+//     const response = await fetch(herokuUrl, {
+//       method: 'PUT',
+//       body: JSON.stringify(activeShoe),
+//       headers: { 'Content-Type': 'application/json' },
+//     });
   
-    if (response.ok) {
-      console.log(`Deck status updated!`);
-    } else {
-      console.error(err);
-    }
-  };
-  buildDeck();
-};
-  module.exports = { buildDeck };
+//     if (response.ok) {
+//       console.log(`Deck status updated!`);
+//     } else {
+//       console.error(err);
+//     }
+//   };
+//   buildDeck();
+// };
+//   module.exports = { buildDeck };
 
 // const buildDeck = async () => {
 //   const activeShoe = new Deck();
@@ -37,11 +37,11 @@ const buildDeck = async () => {
 //   }
 // }
 
-// const buildDeck = async () => {
-//   const activeShoe = new Deck();
-//   const filePath = path.join(__dirname, '../../db/activeShoe.json');
-//   fs.writeFileSync(filePath, JSON.stringify(activeShoe));
-// };
+const buildDeck = async () => {
+  const activeShoe = new Deck();
+  const filePath = path.join(__dirname, '../../db/activeShoe.json');
+  fs.writeFileSync(filePath, JSON.stringify(activeShoe));
+};
 
-// buildDeck();
-// module.exports = { buildDeck } 
+buildDeck();
+module.exports = { buildDeck } 
